c9ef918de42dab50f50df190649988178f9641e8
구본철 <bbon72@idino.co.kr>
2018-09-11 15:26:34 +0900

    대상 .NET Framework 변경

--- Messaging.App/ProcessActivator.cs
+++ Messaging.App/ProcessActivator.cs
@@ -9,7 +9,6 @@ using System.Messaging;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using Messaging.App.Abstractions;
 
 namespace Messaging.App
